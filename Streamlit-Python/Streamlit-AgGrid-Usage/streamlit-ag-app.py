13b319af6c663be45b7d37ffcfe16f85ff4ed25d
Avra <96898985+avrabyt@users.noreply.github.com>
2022-12-22 11:58:35 +0100

    Update streamlit-ag-app.py

--- Streamlit-Python/Streamlit-AgGrid-Usage/streamlit-ag-app.py
+++ Streamlit-Python/Streamlit-AgGrid-Usage/streamlit-ag-app.py
@@ -22,13 +22,13 @@ _funct = st.sidebar.radio(label="Functions", options = ['Display','Highlight','D
 with st.sidebar:
     st.markdown('''
                 ## Relevant Post : 
-            I published a Meidum Article on ,
-            ```
-            Automate Streamlit Web App using Interactive AgGrid with Google Sheets
-            ```
-            [here's](https://medium.com/towards-data-science/automate-streamlit-web-app-using-interactive-aggrid-with-google-sheets-81b93fd9e648) the link to the article.
-            
-            '''
+                I published a Meidum Article on ,
+                ```
+                Automate Streamlit Web App using Interactive AgGrid with Google Sheets
+                ```
+                [here's](https://medium.com/towards-data-science/automate-streamlit-web-app-using-interactive-aggrid-with-google-sheets-81b93fd9e648) the link to the article.
+
+            ''')
 
 st.header("This is AgGrid Table")
 
